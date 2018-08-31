99ebba3a86896ad189f1c87c3456789dbc78a488
heathdutton <hdutton@thedmsgrp.com>
2018-08-31 13:04:19 -0400

    Increase cron frequencies.

--- web/api/pulls/index.php
+++ web/api/pulls/index.php
@@ -8,22 +8,22 @@ $pool       = new Cache\Adapter\Apcu\ApcuCachePool();
 $simplified = $pool->get($key);
 $simplified = false;
 if (!$simplified) {
-    $simplified            = [];
-    $client = new Github\Client();
-    $pager  = new Github\ResultPager($client);
+    $simplified = [];
+    $client     = new Github\Client();
+    $pager      = new Github\ResultPager($client);
     $client->addCache($pool, ['default_ttl' => $ttl]);
     $client->authenticate(getenv('GH_TOKEN'), null, Github\Client::AUTH_HTTP_TOKEN);
 
     // Get all open PRs sorted by popularity.
-    $params                = [
+    $params  = [
         'state'     => 'open',
         'base'      => 'staging',
         'sort'      => 'popularity',
         'direction' => 'desc',
         'per_page'  => 100,
     ];
-    $repoApi               = $client->api('pullRequest');
-    $pulls                 = $pager->fetch($repoApi, 'all', ['mautic', 'mautic', $params]);
+    $repoApi = $client->api('pullRequest');
+    $pulls   = $pager->fetch($repoApi, 'all', ['mautic', 'mautic', $params]);
     while (!empty($pulls)) {
         foreach ($pulls as $pull) {
             $simplified[(string) $pull['number']] = [
@@ -39,10 +39,23 @@ if (!$simplified) {
     $pool->set($key, $simplified, $ttl);
 }
 
+// Discern if we are coming from a github issue.
+$suggestion = null;
+if (!empty($_SERVER['HTTP_REFERER']) && false !== strpos(
+        $_SERVER['HTTP_REFERER'],
+        'github.com/mautic/mautic/'
+    )) {
+    $parts = explode('/', str_replace('https://github.com/mautic/mautic/', '', $_SERVER['HTTP_REFERER']));
+    if (isset($parts[0]) && is_numeric($parts[0])) {
+        $suggestion = (int) $parts[0];
+    }
+}
+
 outputResult(
     [
-        'error' => null,
-        'pulls' => $simplified,
+        'error'      => null,
+        'pulls'      => $simplified,
+        'suggestion' => $suggestion,
     ]
 );
 
