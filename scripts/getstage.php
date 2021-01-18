1457a469b177bc5b470c6a7ea245e72cee989992
heathdutton <hdutton@thedmsgrp.com>
2021-01-18 10:07:46 -0500

    Add script for obtaning base branch for PRs.

---
+++ scripts/getstage.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Get stage branch for a given PR number.
+ */
+require_once __DIR__.'/../vendor/autoload.php';
+
+$key        = 'mautic_pulls';
+$ttl        = 300;
+$pool       = new Cache\Adapter\Apcu\ApcuCachePool();
+$simplified = $pool->get($key);
+if (!$simplified) {
+    $simplified = [];
+    $client     = new Github\Client();
+    $pager      = new Github\ResultPager($client);
+    $client->addCache($pool, ['default_ttl' => $ttl]);
+    $client->authenticate(getenv('GH_TOKEN'), null, Github\Client::AUTH_HTTP_TOKEN);
+
+    // Get all open PRs sorted by popularity.
+    $params  = [
+        'state'     => 'open',
+        'sort'      => 'popularity',
+        'direction' => 'desc',
+        'per_page'  => 100,
+    ];
+    $repoApi = $client->api('pullRequest');
+    $pulls   = $pager->fetch($repoApi, 'all', ['mautic', 'mautic', $params]);
+    while (!empty($pulls)) {
+        foreach ($pulls as $pull) {
+            // Mautic 2 branches no longer supported.
+            if (0 === strpos($pull['base']['ref'], '2.')) {
+                continue;
+            }
+            $simplified[(string) $pull['number']] = [
+                'title' => $pull['title'],
+                'user'  => !empty($pull['user']['login']) ? $pull['user']['login'] : '',
+                'base'  => $pull['base']['ref'] ?? getenv('STAGING_BRANCH'),
+            ];
+        }
+        $pulls = [];
+        if ($pager->hasNext()) {
+            $pulls = $pager->fetchNext();
+        }
+    }
+    $pool->set($key, $simplified, $ttl);
+}
+
+if (isset($argv[1]) && isset($simplified[$argv[1]])) {
+    echo $simplified[$argv[1]]['base'];
+}
